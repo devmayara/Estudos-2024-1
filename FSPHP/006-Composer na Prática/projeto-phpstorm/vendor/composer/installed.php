44edf1af439c1a8ce17e01f4d19c1feb1b7615f4
Mayara Silva <devmayara11@gmail.com>
2023-12-25 21:25:39 -0300

    composer no phpstorm

---
+++ FSPHP/006-Composer na Prática/projeto-phpstorm/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'devmayara/projeto-phpstorm',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd00d1cffa65de89dd98bfad80a8621064aeac399',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'devmayara/projeto-phpstorm' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd00d1cffa65de89dd98bfad80a8621064aeac399',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'gustavoweb/notification' => array(
+            'pretty_version' => 'v1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => '90b5e67719368a10d79600aef1c560a834694f2e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gustavoweb/notification',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.0.1',
+            'version' => '6.0.1.0',
+            'reference' => '992392437c2e2784e0dc41446024fe411d293c96',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
