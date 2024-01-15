43acdd78fd7a6a7406b7e250baddc8c4285cb5ef
Mayara Silva <devmayara11@gmail.com>
2024-01-15 19:12:08 -0300

    Interação com URLs

--- FSPHP/001-FSPHP Developer/03-manipulacao-e-tratamento/03-11-interaco-com-urls/index.php
+++ FSPHP/001-FSPHP Developer/03-manipulacao-e-tratamento/03-11-interaco-com-urls/index.php
@@ -7,9 +7,56 @@ fullStackPHPClassName("03.11 - Interação com URLs");
  */
 fullStackPHPClassSession("argumentos", __LINE__);
 
+echo "<h1><a href='index.php'>Clear</a></h1>";
+echo "<p><a href='index.php?arg1=true&arg2=true'>Argumentos</a></p>";
+
+$data = [
+    "name" => "Robson",
+    "company" => "UpInside",
+    "mail" => "cursos@upinside.com.br"
+];
+
+$arguments = http_build_query($data);
+echo "<p><a href='index.php?{$arguments}'>Argumentos por array</a></p>";
+
+var_dump($_GET);
+
+$object = (object)$data;
+var_dump(
+    $object,
+    http_build_query($object)
+);
+
 
 /*
  * [ segurança ] get | strip | filters | validation
  * [ filter list ] https://php.net/manual/en/filter.filters.php
  */
 fullStackPHPClassSession("segurança", __LINE__);
+
+$dataFilter = http_build_query([
+    "name" => "Robson",
+    "company" => "UpInside",
+    "mail" => "cursos@upinside.com.br",
+    "site" => "upinside.com.br",
+    "script" => "<script>alert('code js!')</script>"
+]);
+echo "<p><a href='index.php?{$dataFilter}'>Data Filter</a></p>";
+
+$dataUrl = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+if ($dataUrl) {
+    if (in_array("", $dataUrl)) {
+        echo "<p class='trigger warning'>Faltam dados!</p>";
+    } elseif (empty($dataUrl["mail"])) {
+        echo "<p class='trigger warning'>Falta o e-mail!</p>";
+    } elseif (!filter_var($dataUrl["mail"], FILTER_VALIDATE_EMAIL)) {
+        echo "<p class='trigger warning'>E-mail inválido!</p>";
+    } else {
+        echo "<p class='trigger accept'>Tudo certo!</p>";
+    }
+} else {
+    var_dump(false);
+}
+
+var_dump($dataUrl);
+
