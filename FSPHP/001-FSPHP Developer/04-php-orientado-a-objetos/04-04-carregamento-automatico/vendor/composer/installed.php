7b82c7db4ec06bcdead858518f63e373b4c5662e
Mayara Silva <devmayara11@gmail.com>
2024-01-17 19:19:18 -0300

    Carregamento automático

---
+++ FSPHP/001-FSPHP Developer/04-php-orientado-a-objetos/04-04-carregamento-automatico/vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'name' => 'devmayara/fsphp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f9ee9543e79a4ce9d58172e8d551091ea2d555c5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'devmayara/fsphp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f9ee9543e79a4ce9d58172e8d551091ea2d555c5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.1',
+            'version' => '6.9.1.0',
+            'reference' => '039de174cd9c17a8389754d3b877a2ed22743e18',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'roave/security-advisories' => array(
+            'pretty_version' => 'dev-latest',
+            'version' => 'dev-latest',
+            'reference' => 'f81bd7cb90f5f30d3b246e342843ae905947158f',
+            'type' => 'metapackage',
+            'install_path' => NULL,
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => true,
+        ),
+    ),
+);
