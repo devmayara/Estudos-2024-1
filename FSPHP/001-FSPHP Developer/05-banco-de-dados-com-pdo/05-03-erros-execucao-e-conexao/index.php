81c5c4bd0cb23469752ea930bbe289edc98b74aa
Mayara Silva <devmayara11@gmail.com>
2024-01-21 19:01:19 -0300

    Errors, conexão e execução

--- FSPHP/001-FSPHP Developer/05-banco-de-dados-com-pdo/05-03-erros-execucao-e-conexao/index.php
+++ FSPHP/001-FSPHP Developer/05-banco-de-dados-com-pdo/05-03-erros-execucao-e-conexao/index.php
@@ -1,4 +1,7 @@
 <?php
+
+use Source\Database\Connect;
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("05.03 - Errors, conexão e execução");
 
@@ -7,6 +10,19 @@ fullStackPHPClassName("05.03 - Errors, conexão e execução");
  */
 fullStackPHPClassSession("controle de erros", __LINE__);
 
+try {
+//    throw new Exception("Exeception");
+//    throw new PDOException("PDOException");
+    throw new ErrorException("PDOException");
+} catch (PDOException | ErrorException $e) {
+    var_dump($e);
+} catch (Exception $e) {
+    echo "<p class='trigger error'>{$e->getMessage()}</p>";
+} finally {
+    echo "<p class='trigger'>Execução terminou!</p>";
+}
+
+
 
 /*
  * [ php data object ] Uma classe PDO para manipulação de banco de dados.
@@ -14,9 +30,40 @@ fullStackPHPClassSession("controle de erros", __LINE__);
  */
 fullStackPHPClassSession("php data object", __LINE__);
 
+try {
+    $pdo = new PDO(
+        "mysql:host=localhost;dbname=fullstackphp",
+        "root",
+        "",
+        [
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        ]
+    );
+
+    $stmt = $pdo->query("SELECT * FROM users LIMIT 3");
+    while ($user = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        var_dump($user);
+    }
+} catch (PDOException $e) {
+    var_dump($e);
+}
+
 
 /*
  * [ conexão com singleton ] Conextar e obter um objeto PDO garantindo instância única.
  * http://br.phptherightway.com/pages/Design-Patterns.html
  */
 fullStackPHPClassSession("conexão com singleton", __LINE__);
+
+require __DIR__ . "/../source/autoload.php";
+$pdo1 = Connect::getInstance();
+$pdo2 = Connect::getInstance();
+
+var_dump(
+    $pdo1,
+    $pdo2,
+    Connect::getInstance(),
+    Connect::getInstance()::getAvailableDrivers(),
+    Connect::getInstance()->getAttribute(PDO::ATTR_DRIVER_NAME)
+);
+
