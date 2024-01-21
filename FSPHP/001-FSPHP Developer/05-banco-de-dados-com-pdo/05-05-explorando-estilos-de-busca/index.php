c6405391a873992382204cdc0a3cdaa7f46b62d2
Mayara Silva <devmayara11@gmail.com>
2024-01-21 20:28:45 -0300

    Explorando estilos de busca

--- FSPHP/001-FSPHP Developer/05-banco-de-dados-com-pdo/05-05-explorando-estilos-de-busca/index.php
+++ FSPHP/001-FSPHP Developer/05-banco-de-dados-com-pdo/05-05-explorando-estilos-de-busca/index.php
@@ -5,17 +5,45 @@ fullStackPHPClassName("05.05 - Explorando estilos de busca");
 require __DIR__ . "/../source/autoload.php";
 
 use Source\Database\Connect;
+use Source\Database\Entity\UserEntity;
 
 /*
  * [ fetch ] http://php.net/manual/pt_BR/pdostatement.fetch.php
  */
 fullStackPHPClassSession("fetch", __LINE__);
 
+$connect = Connect::getInstance();
+$read = $connect->query("SELECT * FROM users LIMIT 3");
+
+if (!$read->rowCount()) {
+    echo "<p class='trigger warning'>Não obteve resultados.</p>";
+} else {
+//    var_dump($read->fetch());
+
+    while ($user = $read->fetch()) {
+        var_dump($user);
+    }
+
+    var_dump($read->fetch());
+}
+
 
 /*
  * [ fetch all ] http://php.net/manual/pt_BR/pdostatement.fetchall.php
  */
 fullStackPHPClassSession("fetch all", __LINE__);
+
+$read = $connect->query("SELECT * FROM users LIMIT 3,2");
+
+//while ($user = $read->fetchAll()) {
+//    var_dump($user);
+//}
+
+foreach ($read->fetchAll() as $user) {
+    var_dump($user);
+}
+
+var_dump($read->fetchAll());
 
 
 /*
@@ -24,10 +52,41 @@ fullStackPHPClassSession("fetch all", __LINE__);
  */
 fullStackPHPClassSession("fetch save", __LINE__);
 
+$read = $connect->query("SELECT * FROM users LIMIT 5,1");
+$result = $read->fetchAll();
+
+var_dump(
+    $read->fetchAll(),
+    $result,
+    $result
+);
+
 
 /*
  * [ fetch modes ] http://php.net/manual/pt_BR/pdostatement.fetch.php
  */
 fullStackPHPClassSession("fetch styles", __LINE__);
 
+$read = $connect->query("SELECT * FROM users LIMIT 1");
+foreach ($read->fetchAll() as $user) {
+    var_dump($user, $user->first_name);
+}
+
+$read = $connect->query("SELECT * FROM users LIMIT 1");
+foreach ($read->fetchAll(PDO::FETCH_NUM) as $user) {
+    var_dump($user, $user[1]);
+}
+
+$read = $connect->query("SELECT * FROM users LIMIT 1");
+foreach ($read->fetchAll(PDO::FETCH_ASSOC) as $user) {
+    var_dump($user, $user['first_name']);
+}
+
+$read = $connect->query("SELECT * FROM users LIMIT 1");
+foreach ($read->fetchAll(PDO::FETCH_CLASS, UserEntity::class) as $user) {
+    /**
+     * @var UserEntity $user
+     */
+    var_dump($user, $user->getFirstName());
+}
 
