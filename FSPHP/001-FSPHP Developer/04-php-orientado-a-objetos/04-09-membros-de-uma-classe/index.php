5516eb45a680365c7aad321495b53a6474ed1567
Mayara Silva <devmayara11@gmail.com>
2024-01-21 16:45:06 -0300

    Membros de uma classe

--- FSPHP/001-FSPHP Developer/04-php-orientado-a-objetos/04-09-membros-de-uma-classe/index.php
+++ FSPHP/001-FSPHP Developer/04-php-orientado-a-objetos/04-09-membros-de-uma-classe/index.php
@@ -1,4 +1,7 @@
 <?php
+
+use Source\Members\Config;
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("04.09 - Membros de uma classe");
 
@@ -9,11 +12,32 @@ require __DIR__ . "/source/autoload.php";
  */
 fullStackPHPClassSession("constantes", __LINE__);
 
+$config = new Config();
+echo "<p>". $config::COMPANY ."</p>";
+
+var_dump(
+    Config::COMPANY
+//    Config::DOMAIN,
+//    Config::SECTOR
+);
+
+$reflection = new Reflection(Config::class);
+
+//var_dump($config, $reflection->getConstants());
+
 
 /*
  * [ propriedades ] http://php.net/manual/pt_BR/language.oop5.static.php
  */
 fullStackPHPClassSession("propriedades", __LINE__);
+
+Config::$company = "UpInside";
+Config::$domain = "upinside.com.br";
+Config::$sector = "Educação";
+
+$config::$sector = "tecnologia";
+
+//var_dump($config, $reflection->getMethods(), $reflection->getDefaultProperties());
 
 
 /*
@@ -21,8 +45,16 @@ fullStackPHPClassSession("propriedades", __LINE__);
  */
 fullStackPHPClassSession("métodos", __LINE__);
 
+$config::setConfig("", "", "");
+Config::setConfig("UpInside", "upinside.com.br", "Educação");
+
+var_dump($config, $reflection->getMethods(), $reflection->getDefaultProperties());
+
 
 /*
  * [ exemplo ] Uma classe trigger
  */
 fullStackPHPClassSession("exemplo", __LINE__);
+
+
+
