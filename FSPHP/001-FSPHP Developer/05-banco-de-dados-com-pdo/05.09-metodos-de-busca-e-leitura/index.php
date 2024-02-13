debee466f73f10428a677092e72c6dde80dceea9
Mayara Silva <devmayara11@gmail.com>
2024-02-13 15:16:36 -0300

    Métodos de busca e leitura

--- FSPHP/001-FSPHP Developer/05-banco-de-dados-com-pdo/05.09-metodos-de-busca-e-leitura/index.php
+++ FSPHP/001-FSPHP Developer/05-banco-de-dados-com-pdo/05.09-metodos-de-busca-e-leitura/index.php
@@ -1,4 +1,7 @@
 <?php
+
+use Source\Models\UserModel;
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("05.09 - Métodos de busca e leitura");
 
@@ -9,15 +12,31 @@ require __DIR__ . "/../source/autoload.php";
  */
 fullStackPHPClassSession("load", __LINE__);
 
+$model = new UserModel();
+
+$user = $model->load(1);
+var_dump($user, "{$user->first_name} {$user->last_name}");
+
 
 /*
  * [ find ] Por indexes da tabela (email)
  */
 fullStackPHPClassSession("find", __LINE__);
 
+$user = $model->find("joão32@email.com.br");
+var_dump($user, "{$user->first_name} {$user->last_name}");
+
 
 /*
  * [ all ] Retorna diversos registros
  */
 fullStackPHPClassSession("all", __LINE__);
+
+$all = $model->all(limit: 5);
+/**
+ * @var UserModel $user
+ */
+foreach ($all as $user) {
+    var_dump($user, "{$user->first_name} {$user->last_name}");
+}
 
