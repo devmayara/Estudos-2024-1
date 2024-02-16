e98f38dd4f8928ba6e76e3797914b8f7f3a7454e
Mayara Silva <devmayara11@gmail.com>
2024-02-15 21:02:23 -0300

    Segurança e gestão de senhas

--- FSPHP/001-FSPHP Developer/06-seguranca-e-boas-praticas/06-09-seguranca-e-gestao-de-senhas/index.php
+++ FSPHP/001-FSPHP Developer/06-seguranca-e-boas-praticas/06-09-seguranca-e-gestao-de-senhas/index.php
@@ -1,13 +1,28 @@
 <?php
+
+use Source\Models\User;
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("06.09 - Segurança e gestão de senhas");
 
 require __DIR__ . "/../source/autoload.php";
 
+session();
+
 /*
  * [ password hashing ] Uma API PHP para gerenciamento de senhas de modo seguro.
  */
 fullStackPHPClassSession("password hashing", __LINE__);
+
+//$pass = password_hash(12345, PASSWORD_DEFAULT, ["cost" => 12]);
+$pass = password_hash(12345, PASSWORD_DEFAULT);
+var_dump($pass);
+
+var_dump([
+    password_get_info($pass),
+    password_needs_rehash($pass, PASSWORD_DEFAULT, ["cost" => 10]),
+    password_verify(12345, $pass)
+]);
 
 
 /*
@@ -15,14 +30,48 @@ fullStackPHPClassSession("password hashing", __LINE__);
  */
 fullStackPHPClassSession("password saving", __LINE__);
 
+$user = (new User())->load(1);
+$user->password = $pass;
+$user->save();
+
+var_dump(password_verify(12345, $user->password));
+
+var_dump($user);
+
 
 /*
  * [ password verify ] Rotina de vetificação de senha
  */
 fullStackPHPClassSession("password verify", __LINE__);
 
+$login = (new User())->find("robson1@email.com.br");
+var_dump($login);
+
+if (!$login) {
+    echo message()->info("E-mail informado não confere!");
+} elseif (!password_verify(12345, $login->password)) {
+    echo message()->error("Senha não confere!");
+} else {
+    $login->password = password_hash(12345, PASSWORD_DEFAULT);
+    $login->save();
+
+    session()->set("Login", $login->data());
+
+    echo message()->success("Bem vindo(a) de volta {$login->first_name}!");
+    var_dump(session()->all());
+}
+
+
 
 /*
  * [ password handler ] Sintetizando uso de senhas
  */
 fullStackPHPClassSession("password handler", __LINE__);
+
+$pass = "289435823";
+var_dump([
+    $passwd = passwd($pass),
+    passwd_verify($pass, $passwd),
+    passwd_rehash($passwd)
+]);
+
--- FSPHP/001-FSPHP Developer/06-seguranca-e-boas-praticas/source/Support/Config.php
+++ FSPHP/001-FSPHP Developer/06-seguranca-e-boas-praticas/source/Support/Config.php
@@ -31,6 +31,8 @@ define("CONF_SES_PATH", __DIR__ . "/../../storage/sessions/");
  */
 define("CONF_PASSWD_MIN_LEN", 8);
 define("CONF_PASSWD_MAX_LEN", 40);
+define("CONF_PASSWD_ALGO", PASSWORD_DEFAULT);
+define("CONF_PASSWD_OPTION", ["cost" => 10]);
 
 /**
  * MESSAGE
